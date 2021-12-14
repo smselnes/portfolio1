e6cec6598d7e34a0d17fff3ebb744b39cd5ca1a2
smselnes <smselnes@gmail.com>
2021-12-14 13:23:06 +0100

    Created Repo
    First upload of files to repository.
    Updated readme.

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/* const crossCourseButton = document.querySelector(".cross-course-btn");
+const crossCourseLink = document.querySelector(".cross-course-link");
+const crossCourseChanges = document.querySelector(".cross-course-changes");
+
+crossCourseButton.addEventListener("click", showCrossCourseButtons);
+
+function showCrossCourseButtons() {
+    crossCourseLink.classList.toggle("show-links");
+    crossCourseChanges.classList.toggle("show-links");
+    crossCourseButton.classList.toggle("expand");
+}
+const semesterProjectButton = document.querySelector(".semester-project-btn");
+const semesterProjectLink = document.querySelector(".semester-project-link");
+const semesterProjectChanges = document.querySelector(".semester-project-changes");
+
+semesterProjectButton.addEventListener("click", showSemesterProjectButtons);
+
+function showSemesterProjectButtons() {
+    semesterProjectLink.classList.toggle("show-links");
+    semesterProjectChanges.classList.toggle("show-links");
+    semesterProjectButton.classList.toggle("expand");
+} */
---
+++ readme.txt
@@ -0,0 +1,18 @@
+Recent changes: 
+13.12.21: Adjusting the use of shadows on buttons, links and images in semester project.
+14.12.21: Added form validation to cross-course project.
+14.12.21: Media queries on portfolio.
+14.12.21: Removing images and icons not used in all projects.
+
+Future improvements:
+Cross course project:
+-Add preview of content on mainpage? More engaging.
+
+Semester project: 
+-Fix borders shifting content around. 
+
+Project exam:
+-Level 2 process. 
+-Better styling on contact form. (I think it is a bit cluttering)
+-Improved javascript on homepage navigation and blog posts page. 
+-Removing logo. No function.
